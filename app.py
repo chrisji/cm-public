576f36e8a0859333d97c45494460091a6acbbfea
chrisji <chrisji@hotmail.co.uk>
2025-11-22 12:31:01 +0000

    Update description - typos

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ DATA_FIELD_RENAMES = {
 DASHBOARD_DESCRIPTION = """
 This dashboard provides an interactive visualisation of Estonian Facebook and Telegram messages posted from January 2020 to November 2023, focusing on conspiracy-related content. The data was collected from public groups and channels that discuss conspiracy theories.
 
-Messages were processed via natural language processing (NLP) techniques to (a) infer their relevance to conspiracy theory discourse and (b) assign thematic categories. Relevant messages are further represented in two-dimensions using a dimensionality reduction technique that attempts to preserve the semantic relationships (i.e., two messages that are visusally close share similar meanings). A total of 56,290 messages are included in the visualisation.
+Messages were processed via natural language processing (NLP) techniques to (a) infer their relevance to conspiracy theory discourse and (b) assign thematic categories. Relevant messages are further represented in two-dimensions using a dimensionality reduction technique that attempts to preserve the semantic relationships (i.e., two messages that are visually close share similar meanings). A total of 56,290 messages are included in the visualisation.
 
-The visualisation provides an interactive animation of how messagaging and the thematic categories change over time. Animation Controls can be used to change the speed, granularity / smoothing, and category being displayed, and Data Filters can be used to focus in on specific thematic categories, conspiracy theory relevancy, and platforms.
+The visualisation provides an interactive animation of how messaging and the thematic categories change over time. Animation Controls can be used to change the speed, granularity / smoothing, and category being displayed, and Data Filters can be used to focus in on specific thematic categories, conspiracy theory relevancy, and platforms.
 """
 
 st.set_page_config(
