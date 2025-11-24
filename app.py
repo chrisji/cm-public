48b49501a506cb6088702d5b11299470664d484a
Chris Inskip <chrisji@hotmail.co.uk>
2025-11-24 15:37:49 +0000

    Add project attribution to overview

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ Messages were processed via natural language processing (NLP) techniques to (a) 
 Relevant messages are further represented in two-dimensions using a dimensionality reduction technique that attempts to preserve the semantic relationships (i.e., two messages that are visually close share similar meanings). A total of 56,290 messages are included in the visualisation.
 
 The visualisation provides an interactive animation of how messaging and the thematic categories changes over time. The animation can be interacted with directly to e.g., zoom into regions, skip to specific time frames, make it fullscreen. Further controls in the sidebar can be used to change the speed, granularity, and category being displayed, and Data Filters can be used to focus in on specific thematic categories, conspiracy theory relevancy, and platforms.
+
+This dashboard was created by [CASM Technology](https://casmtechnology.com/) in collaboration with the [REDACT](https://redactproject.sites.er.kcl.ac.uk/) project. REDACT is supported by the CHANSE ERA-NET Co-fund programme, which has received funding from the European Union’s Horizon 2020 Research and Innovation Programme, under Grant Agreement no. 101004509.
 """
 
 # Theme color map (avoid duplicate colours)
